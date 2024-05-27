7bde773acd27d0e0cb77802dd4987e680e1c24d8
emmadal <emmanueldalougou@gmail.com>
2024-05-27 18:21:26 +0000

    add localization config

---
+++ i18n.ts
@@ -0,0 +1,25 @@
+import { getLocales } from "expo-localization";
+import { I18n } from "i18n-js";
+import * as en from "./locales/en.json";
+
+// Set the key-value pairs for the different languages you want to support.
+const translations = {
+  en,
+};
+const i18n = new I18n(translations);
+
+const CheckPhoneLanguage = () => {
+  const lang = getLocales()[0].languageCode;
+  if (lang === "en" || lang === "fr") {
+    return getLocales()[0].languageCode as string;
+  }
+  return "en";
+};
+
+// Set the locale once at the beginning of your app.
+i18n.locale = CheckPhoneLanguage();
+
+// When a value is missing from a language it'll fall back to another language with the key present.
+i18n.enableFallback = true;
+
+export default i18n;
