4a51b4004a189465adaee05d540dfaebc19f59c5
emmadal <emmanueldalougou@gmail.com>
2024-12-04 05:39:36 +0000

    feat: add scrollview layout for app content

---
+++ components/ParallaxScroll.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { ScrollView, useWindowDimensions } from "react-native";
+
+export default function ParallaxScroll({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { height } = useWindowDimensions();
+  return (
+    <ScrollView
+      style={{ flex: 1 }}
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+      scrollEventThrottle={200}
+      decelerationRate="fast"
+      snapToInterval={height}
+      snapToAlignment="start"
+      automaticallyAdjustContentInsets
+      contentInsetAdjustmentBehavior="automatic"
+      className="p-5 dark:bg-black bg-white"
+      testID="parallax-scroll-view"
+    >
+      {children}
+    </ScrollView>
+  );
+}
