3f77da4a2f670e40a46a2b146e49fc6faad5a8ba
emmadal <emmanueldalougou@gmail.com>
2024-12-04 05:43:45 +0000

    chore: update locale files and project dependencies

---
+++ data/about.ts
@@ -0,0 +1,16 @@
+import { User } from "@/store/type";
+
+export const user: User = {
+  id: 1,
+  username: "johndoe",
+  email: "ZV5sM@example.com",
+  location: "New York",
+  biography:
+    "I'm a software engineer with over 10 years of experience. I love to code and create things that make people's lives easier.",
+  avatar_url:
+    "https://pbs.twimg.com/profile_images/1763627121350524928/_iXENE0l_400x400.jpg",
+  name: "John Doe",
+  premium: true,
+  created_at: "2024-05-03T17:06:41.577Z",
+  website: "https://emmadal.vercel.app",
+};
