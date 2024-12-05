5458e25c2ecc13ea9c554485104aaaf7c8722572
emmadal <emmanueldalougou@gmail.com>
2024-12-05 11:59:26 +0000

    refactor: refactor _layout in (home) group

--- app/(home)/_layout.tsx
+++ app/(home)/_layout.tsx
@@ -1,20 +1,27 @@
+import i18n from "@/i18n";
 import { Stack } from "expo-router";
 
 export default function HomeLayout() {
   return (
-    <Stack screenOptions={{ animation: "slide_from_bottom" }}>
+    <Stack
+      screenOptions={{
+        animation: "slide_from_bottom",
+        headerBackButtonDisplayMode: "minimal",
+        headerShadowVisible: false,
+        headerTitleAlign: "center",
+      }}
+    >
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="profile" options={{ title: i18n.t("profile") }} />
+      <Stack.Screen name="settings" options={{ title: i18n.t("settings") }} />
       <Stack.Screen
-        name="profile"
-        options={{
-          autoHideHomeIndicator: true,
-          headerBackButtonDisplayMode: "minimal",
-          headerShadowVisible: false,
-          headerTitleAlign: "center",
-        }}
+        name="notification"
+        options={{ title: i18n.t("notification") }}
       />
-      <Stack.Screen name="settings" options={{ title: "Settings" }} />
-      <Stack.Screen name="notification" options={{ title: "Notifications" }} />
+      <Stack.Screen
+        name="edit-profile"
+        options={{ title: i18n.t("editprofile") }}
+      />
     </Stack>
   );
 }
